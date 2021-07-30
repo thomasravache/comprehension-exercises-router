f5fbed540964cbc3266c689cff1f825ca7095d24
thomasravache <thomasravache31@gmail.com>
2021-07-30 15:13:58 -0300

    Exercicio 02 - OK

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home /> 
+        <Route path="/">
+          <Home /> 
+        </Route>
       </BrowserRouter>
     );
   }
